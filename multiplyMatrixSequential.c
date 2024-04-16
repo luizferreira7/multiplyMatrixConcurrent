acf5c6605fcde525627edd284aaaf1d3a17d2a49
Luiz Carlos Ferreira <84511085+luizferreira7@users.noreply.github.com>
2024-04-16 11:39:07 -0300

    Adiciona medidas de tempo e escrita para CSV

--- multiplyMatrixSequential.c
+++ multiplyMatrixSequential.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include "timer.h"
 
 // Estrutura usada para representar uma matriz
 typedef struct {
@@ -12,20 +12,8 @@ typedef struct {
 // Caso queira imprimir as matrizes descomentar
 // #define PRINT
 
-// Define tamanho de 10ˆ9 para calculo de tempo (conversao de nanosegundos)
-#define BILLION 1000000000
-
 // Metodo que multiplica duas matrizes de dimensões NxM por M*K
-void multiplyMatrix(Matrix * matrixA, Matrix * matrixB, Matrix * matrixC) {\
-
-    if (matrixA->cols != matrixB->rows) {
-        printf("Erro, para a multiplicação ser possivel o numero de colunas de A deve ser igual ao numero de linhas de B.");
-        return;
-    }
-
-    struct timespec start, end;
-
-    clock_gettime(CLOCK_MONOTONIC, &start);
+void multiplyMatrix(Matrix * matrixA, Matrix * matrixB, Matrix * matrixC) {
 
     for (int i = 0; i < matrixC -> rows; i++) {
         for (int j = 0; j < matrixC -> cols; j++) {
@@ -35,13 +23,6 @@ void multiplyMatrix(Matrix * matrixA, Matrix * matrixB, Matrix * matrixC) {\
             }
         }
     }
-
-    clock_gettime(CLOCK_MONOTONIC, &end);
-
-    int64_t diff = ((int64_t)end.tv_sec - (int64_t)start.tv_sec) * (int64_t) BILLION
-                   + ((int64_t)end.tv_nsec - (int64_t)start.tv_nsec);
-
-    printf("#Tempo (sequencial): %f segundos\n", (float)  diff / BILLION);
 }
 
 // Metodo que lê a matriz a partir de um arquivo binário
@@ -97,6 +78,7 @@ Matrix *readMatrixFromFile(char * filename) {
     return matrix;
 }
 
+// Metodo que imprime a matriz
 void printMatrix(Matrix *matrix) {
     for(int i=0; i<matrix -> rows; i++) {
         for(int j=0; j<matrix -> cols; j++)
@@ -105,6 +87,7 @@ void printMatrix(Matrix *matrix) {
     }
 }
 
+// Metodo que escreve a mtriz em um arquivo binário
 void writeMatrixToFile(Matrix * matrix, char * filename) {
 
     size_t ret;
@@ -129,42 +112,78 @@ void writeMatrixToFile(Matrix * matrix, char * filename) {
     fclose(file);
 }
 
+
+// Escreve os resultados no CSV
+void writeToCSV(char *filename, char tipo, int numThreads, int rows, int cols, double init, double process, double end) {
+
+    char *header = "dimensao,tipo,threads,inicialização,processamento,finalização";
+
+    FILE *file;
+
+    // Verifica se arquivo existe
+    file = fopen(filename,"r");
+
+    // Caso não exista cria o arquivo e escreve o header
+    if (!file) {
+        file = fopen(filename, "w");
+        fprintf(file, "%s\n", header);
+        fclose(file);
+    }
+
+    // Prepara o arquivo para acrescentar novas linhas
+    file = fopen(filename, "a");
+
+    fprintf(file, "%dx%d,%c,%d,%lf,%lf,%lf\n", rows, cols, tipo, numThreads, init, process, end);
+
+    fclose(file);
+}
+
+// Metodo main que executa a multiplicação de matrizes de forma sequencial
 int main(int argc, char*argv[]) {
 
+    double startInit, finishInit, elapsedInit;
+    GET_TIME(startInit);
+
+    // Verifica argumentos passados para o programa
     if(argc < 2) {
         fprintf(stderr, "Digite: ./main <arquivoMatrixA> <arquivoMatrixB>\n");
         return -1;
     }
 
+    // Le a primeira matriz
     Matrix *matrixA = readMatrixFromFile(argv[1]);
     if (!matrixA) {
-        printf("Erro ao ler matrixA do arquivo\n");
+        fprintf(stderr, "Erro ao ler matrixA do arquivo\n");
         free(matrixA);
         return -2;
     }
 
+    // Le a segunda matriz
     Matrix *matrixB = readMatrixFromFile(argv[2]);
     if (!matrixB) {
-        printf("Erro ao ler matrixB do arquivo\n");
+        fprintf(stderr, "Erro ao ler matrixB do arquivo\n");
         free(matrixB);
         return -2;
     }
 
+    // Verifica se as matrizes podem ser multiplicadas
     if (matrixA->cols != matrixB->rows) {
-        printf("Erro, para a multiplicação ser possivel o numero de colunas de A deve ser igual ao numero de linhas de B.");
+        fprintf(stderr, "Erro, para a multiplicação ser possivel o numero de colunas de A deve ser igual ao numero de linhas de B.");
         return -3;
     }
 
+    // Aloca memótia para a matriz resultado
     Matrix *matrixC = malloc(sizeof(Matrix));
     if (!matrixC) {
-        printf("Erro ao alocar memoria para matrixC\n");
+        fprintf(stderr, "Erro ao alocar memoria para matrixC\n");
         free(matrixC);
         return -4;
     }
 
+    // Aloca memoria para os elementos da matriz, representados na forma de um array
     float *mArray = (float *) malloc(sizeof(float) * matrixA->rows * matrixB->cols);
     if (!mArray) {
-        printf("Erro ao alocar memoria para matrixC\n");
+        fprintf(stderr, "Erro ao alocar memoria para matrixC\n");
         free(mArray);
         return -4;
     }
@@ -173,18 +192,48 @@ int main(int argc, char*argv[]) {
     matrixC -> rows = matrixA->rows;
     matrixC -> cols = matrixB->cols;
 
+    int rows = matrixC -> rows;
+    int cols = matrixC -> cols;
+
+    GET_TIME(finishInit);
+    elapsedInit = finishInit - startInit;
+
+    printf("#Tempo (inicialização): %lf segundos\n", elapsedInit);
+
+    double startProcess, finishProcess, elapsedProcess;
+    GET_TIME(startProcess);
+
+    // Executa multiplicação das mtrizes
     multiplyMatrix(matrixA, matrixB, matrixC);
+
+    GET_TIME(finishProcess);
+    elapsedProcess = finishProcess - startProcess;
+
+    printf("#Tempo (processamento): %lf segundos\n", elapsedProcess);
+
+    double startEnd, finishEnd, elapsedEnd;
+    GET_TIME(startEnd);
 
 #ifdef PRINT
     printMatrix(matrixC);
 #endif
 
+    // Escreve matriz resultado para arquivo
     writeMatrixToFile(matrixC, "seqMatrixC");
 
+    // Libera memoria alcoada anteriormente
     free(mArray);
     free(matrixA);
     free(matrixB);
     free(matrixC);
+
+    GET_TIME(finishEnd);
+    elapsedEnd = finishEnd - startEnd;
+
+    printf("#Tempo (finalização): %lf segundos\n", elapsedEnd);
+
+    // Escreve medidas de tempo em CSV
+    writeToCSV("medidasTempo.csv", 'S', 1, rows, cols, elapsedInit, elapsedProcess, elapsedEnd);
 
     return 0;
 }
